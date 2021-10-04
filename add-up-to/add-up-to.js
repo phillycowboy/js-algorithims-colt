772dce511e493f4ab4d9edffe3d8fe20b15cc9bf
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-04 18:34:45 -0400

    changes

--- add-up-to/add-up-to.js
+++ add-up-to/add-up-to.js
@@ -1,3 +1,10 @@
-let msg = "Hello there and welcome to the club!"
+// let msg = "Hello there and welcome to the club!"
 
-console.log(msg)
+// console.log(msg)
+
+const addUpTo = (n) => {
+    let total = 0; 
+    for(let i = 1; i < n; i++){
+        
+    }
+}

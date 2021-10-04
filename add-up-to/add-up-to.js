3438777afd66cbd06f897d7a8aca8ff90fd52a7a
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-04 19:05:52 -0400

    comparing runtimes of functions

--- add-up-to/add-up-to.js
+++ add-up-to/add-up-to.js
@@ -2,6 +2,8 @@
 
 // console.log(msg)
 
+
+// first way to write this function! 
 const addUpTo = (n) => {
     let total = 0; 
     for(let i = 1; i <= n; i++){
@@ -9,5 +11,23 @@ const addUpTo = (n) => {
     }
     return total 
 }
+let t1 = performance.now()
+addUpTo(1000000000)
+let t2 = performance.now()
+console.log(`Time elapsed ${t2 - t1 / 1000} seconds.`)
 
-console.log(addUpTo(100))
+
+// console.log(addUpTo(100))
+
+// second way to write this function! 
+
+const secondAddUpTo = (n) => {
+    return n * (n + 1) / 2
+}
+
+let t3 = performance.now()
+secondAddUpTo(1000000000)
+let t4 = performance.now()
+console.log(`Time elapsed ${t4 - t3 /1000} seconds.`)
+// console.log(secondAddUpTo(100))
+

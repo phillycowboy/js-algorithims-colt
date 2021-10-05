75f8d92ce557359257c2bbdb3421cf0913dd4196
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-05 14:29:52 -0400

    completed algorithim

---
+++ palindrome-algorithim/index.js
@@ -0,0 +1,17 @@
+console.log("Hello there!")
+
+const checkPailndrome = (string) => {
+    // takes in the argument of a string
+    if(string === string.split('').reverse().join('')){
+        return true 
+    }else {
+        return false
+    }
+    // checks the string if it is the same backwards as it is forwards 
+    // if it is return true 
+    // else return false 
+
+}
+
+console.log(checkPailndrome("mom"))
+console.log(checkPailndrome("grandpa"))

661b79fd839c4b52975259873f7f17d0b93ed674
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-06 22:14:42 -0400

    finished time complexiety with big o

--- big-o-notation/index.js
+++ big-o-notation/index.js
@@ -44,4 +44,37 @@ console.log("average of an array", average([1,2,3,4,5,6]))
 //     return total / array.length
 // }
 
-// This gives us a runtime of O(n + 2) we can remove the constants (+2) bc we're just looking for a high level summary 
+// This gives us a runtime of O(n + 2) we can remove the constants (+2) bc we're just looking for a high level summary so we end up with O(n)
+
+// using reduce instead of a for...of loop 
+
+const averageReduce = (array) => {
+    return array.reduce((total, num) => total + num, 0) /array.length
+}
+
+console.log("avearge with reduce", averageReduce([1,2,3,4,5,6]))
+
+// under the hood the reduce method still needs to iterate over every element in the array in the array so we still end up with the same runtime O(n) its important 
+// to keep this in mind with a built in method. just bc we might end up with fewer lines of code doesnt mean our runtime improves 
+
+const containsAverage = (array) => {
+    const averageValue = average(array)
+    for(const num of array){
+        if(num === averageValue){
+            return true
+        }else{
+            return false
+        }
+    }
+}
+
+console.log("contains average", containsAverage([1,2,3,4,5,6]))
+
+// 1 step n steps depending on the size of the array 
+// 2 step n steps depending on the size of the array 
+// counting the steps here we end up with O(n + 2) even though it takes twice as long as our first alogrithim we can still remove the constants and end up with O(n)
+// Since we're only looking for  a high level summary this aproxamation still gives us a good sense as to how well our algorithim will scale to larger and larger 
+// inputs when compared to solve other common Big O runtimes. 
+
+// SPACE COMPLEXIETY WITH BIG O -- 
+

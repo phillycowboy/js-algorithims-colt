fd820e14e815fad9ed2aca2810f1fa57a984f04b
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-06 23:44:06 -0400

    finsihed section

--- big-o-notation/index.js
+++ big-o-notation/index.js
@@ -79,5 +79,51 @@ console.log("contains average", containsAverage([1,2,3,4,5,6]))
 // SPACE COMPLEXIETY WITH BIG O -- 
 
 // -time complexity (how long our function will take to run)
-// -space complexity (how much memory our functio will use)
+// -space complexity (how much memory our function will use)
 
+// just like with time complexity we measure space complexiety relative to the size of the input for an input that takes up n memory, how much memory will our 
+// algo use? in addition to to the amount of memory needed for our inputs we need to consider what auxilary (additional) space is taken up for new data structures 
+// when our algo runs
+
+const reverseString = (word) => {
+    const wordArray = word.split('');
+    const reversedWordArray = wordArray.reverse();
+    const reversedWord = reversedWordArray.join('')
+    return reversedWord;
+}
+
+console.log('reversed string', reverseString("hello there how are you?"))
+
+// in this case we need to allocate memory for the input word as well as these auxilairy pieces of data:
+// - an array wordArray that will grow in size lineraly with the input
+// - an array reversedWordArray that will grow in size lineraly with the input 
+// - a string reversedWord that will grow in size linerly with the input 
+
+
+// Putting that together we end up with O(4n) space complexiety : for an input string length n, we also need to declare two arrays of length n and another string of
+// length n. We can simplify as O(n) after dropping the constants 
+
+// for a function that adds 2 numbers together: 
+
+const sum = (num1, num2) => {
+    return num1 + num2;
+}
+console.log("sum", sum(5,5))
+
+// we only allocate memory for the inputs and the return value :
+//  - a num1 
+//  - a num2 
+//  - a return value 
+// In this case we can summarize the space complexity as constant O(1) 
+
+// In many cases you can improve an algos space complexiety by sacrificing its time complexiety and vice-versa. navigating these trade offs is ont of the keys to being a 
+// good developer and it comes with practice 
+
+// RECAP 
+// big o notation is used to classify alogs according to how their run time or space requirements grow as the input size grows. 
+
+// to calculate the time complexitey of an algorithim using big o notation count the number of steps the computer will take to run our code and then remove any constants
+// so (O(2n + 1) becomes just O(n))
+
+// -to calcualte the space complexiety of an algo using big o notation check what memeory is needed for the inputs as well as any auxiliary data structures and also 
+// remove any constants 

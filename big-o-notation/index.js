a51dad3cfea95c846ade27675ee3998a2c1ba339
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-06 20:09:13 -0400

    added searching algo findSock

--- big-o-notation/index.js
+++ big-o-notation/index.js
@@ -1,1 +1,14 @@
 // start here tomorrow with the examples from yesterday! 
+
+const findSock = (laundry) => {
+    // iterates through an array or data structure to find an item 
+    for(const item of laundry){
+        // if the item matches the given parameter return it at the end of the iteration
+        if(item === "sock") return item
+    }
+}
+
+console.log(findSock(["shirt", "shorts", "sock", "pants"]))
+
+// given an array, the function call has to find n item with n number of iterations giving it a time complexiety of O(n +2)
+// removing the constants it has a runtime of O(n)

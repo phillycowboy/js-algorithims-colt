fe64fb31b9d79da3293e90524701dc0a7a60d87d
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-13 22:34:21 -0400

    finished logarithmic algo

--- big-o-examples/index.js
+++ big-o-examples/index.js
@@ -30,5 +30,68 @@ h1.style.color = "#f2f"
 // put the sock on
 
 
+// This strategy is like opening a dictionary (or a phone book) to try and find a particular word. 
+// If you open to a page that's after the word in the alphabet, you know your word is earlier in the dictionary, so you can quickly narrow down your search.
+
+// Here's a visualization of those steps:
+
+// ["belt", "blouse", "pants", "shirt", "shorts", "sock", "underwear"];
+//                             ^
+//                      is this our sock?
+
+// If that item isn't our sock, we can throw out the first set of items (since we know that "sock" comes later in the alphabet than "shirt"), and start in the middle again:
+
+// ["shorts", "sock", "underwear"];
+//            ^
+//     is this our sock?
+
+// Now we found our sock significantly faster, by cutting the size of our input in half each time we search! This is only possible because our input was sorted.
+
+// Even in the worst case scenario (our sock not being in the pile), we'd need at most 3 iterations to look through an array of 7 items:
+
+
+// ["belt", "blouse", "dress", "pants", "shirt", "shorts", "underwear"];
+// //                             ^
+// ["shirt", "shorts", "underwear"];
+// //           ^
+// ["underwear"];
+//    ^
+
+// This approach benefits us more and more the larger our input size gets, since we can cut the area we're searching approximately in half on each iteration.
+//  What if we had 1000 items to look through? With a linear search, the worst case is that we'd need 1000 steps; with this new approach, we'd need just 10:
+
+// 500    n / 2
+// 250    n / 4
+// 125    n / 8
+// 72     n / 16
+// 36     n / 32
+// 18     n / 64
+// 9      n / 128
+// 4      n / 256
+// 2      n / 512
+// 1      n / 1024
+
+// Here's how our algorithm for finding a sock would look in code:
+
+function findSock(sortedLaundry){
+    let start = 0;
+    let end = sortedLaundry.length;
+
+    while(start <= end){
+        let mid = Math.floor((start + end) / 2);
+
+        if(sortedLaundry[mid] === "sock") return "sock";
+
+        if(sortedLaundry[mid] < "sock"){
+            start = mid + 1;    
+        }else{
+            end = mid - 1;
+        }
+    }
+}
+
+console.log(findSock(["belt", "blouse", "dress", "pants", "shirt", "shorts","sock","underwear"]))
+
+
 
 

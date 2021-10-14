2f32d5a1a2a6f13435fb4842af212124eb5a4f47
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-13 22:18:43 -0400

    notes

--- big-o-examples/index.js
+++ big-o-examples/index.js
@@ -10,3 +10,25 @@ h1.style.color = "#f2f"
 // O(log n)    Logarithmic      Binary Search 
 // O(n)        Linear           Iterating through every element in an array 
 // O(n*to the second power 2) Quadratic  Nested Iteration       
+
+
+// Logarithmic O(log n) Binary Search Algo 
+// Let's revisit our sock finding example and see if we can devise a faster algorithm for finding the sock in our laundry. 
+// In our first example, the sock was in an unsorted pile of laundry, so our only hope of finding it was to go through each item in the laundry pile piece by piece.
+// What if our laundry were laid out on the floor in alphabetical order? Well, having our laundry sorted would give us a better way to approach this problem!
+// laundry as a sorted array ["belt", "blouse", "pants", "shirt", "shorts", "sock", "underwear"];
+
+// If we know that our laundry pile is sorted alphabetically we dont need to start searching from the beginning, we could make a more efficent algorithhim by starting our
+// search in the middle
+
+// check the item in the middle of the pile
+// if the item is before our sock in the alphabet
+// check the first half of the pile
+// otherwise, if the item is after our sock in the alphabet
+// check the second half of the pile
+// otherwise, if that item is our sock
+// put the sock on
+
+
+
+

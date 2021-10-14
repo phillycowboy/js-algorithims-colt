85a098a98a26336fda0f58fff657f9b103ce177a
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-10-13 22:10:57 -0400

    notes on 4 examples

--- big-o-examples/index.js
+++ big-o-examples/index.js
@@ -2,3 +2,11 @@
 
 const h1 = document.querySelector("h1")
 h1.style.color = "#f2f"
+
+// There are four different types of big o examples 
+
+// Big O        Name           Example 
+// O(1)        Constant         Accesing a value in an object by its key
+// O(log n)    Logarithmic      Binary Search 
+// O(n)        Linear           Iterating through every element in an array 
+// O(n*to the second power 2) Quadratic  Nested Iteration       

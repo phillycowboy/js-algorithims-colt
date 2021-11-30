9238df321e9245df4a11cb3301c177f0bbde5e5c
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-11-30 10:16:58 -0500

    added notes

--- visulizing-time-complexities/index.js
+++ visulizing-time-complexities/index.js
@@ -1,2 +1,16 @@
 // visualizing time complexities 
 
+function addUpToFirst(n){
+    let total = 0;
+    for(let i = 1; i <= n; i++){
+        total += i;
+    }
+    return total;
+}
+
+function addUpToSecond(n){
+    return n * (n + 1) / 2
+}
+
+
+// calculated runtime for different numbers determing what n was -- see video 
